f060de251324f506e83704b5c582ae82305e3278
linasbk <lina12345.2002@gmail.com>
2024-02-12 20:45:29 +0100

    gud

---
+++ Makefile
@@ -0,0 +1,13 @@
+all:
+	@mkdir -p /home/$(USER)/data/wordpress
+	@mkdir -p /home/$(USER)/data/mariadb
+	@docker compose -f srcs/docker-compose.yml up --build
+
+clean:
+	@docker compose -f srcs/docker-compose.yml down
+
+fclean: clean
+	@docker system prune -af --volumes
+
+
+re: fclean all
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,18 +1,22 @@
-#!bin/bash
-
-MYSQL_ROOT_PASSWORD="root"
-MYSQL_ADMIN_USER="admin"
-MYSQL_ADMIN_PASSWORD="admin"
+#!/bin/sh
+MYSQL_DATABASE="wordpress"
+MYSQL_USER="lina"
+MYSQL_PASSWORD="lina12"
+MYSQL_ROOT_PASSWORD="lina1234"
 
 # MariaDB Config
 service mariadb start
 sleep 5
-
-mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root -e "CREATE USER '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' WITH GRANT OPTION;"
-mysql -u root -e "FLUSH PRIVILEGES;"
-
+if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
+then 
+	echo "Database already exists"
+else
+    mysql -u root -e "CREATE DATABASE \`${MYSQL_DATABASE}\` ;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+    mysql -u root -e "GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+    echo "port = 3306" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+fi
 service mariadb stop
-
 exec "$@"
