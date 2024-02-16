4acc0aed8d1bdb53a46a7aa4894dfe006ee4a4d2
linasbk <lina12345.2002@gmail.com>
2024-02-16 20:12:36 +0100

    yaarbi

--- Makefile
+++ Makefile
@@ -4,13 +4,10 @@ all:
 	@docker compose -f srcs/docker-compose.yml up --build
 
 clean:
-	@docker compose -f srcs/docker-compose.yml down
+	@docker compose -f srcs/docker-compose.yml down -v
 
 fclean: clean
-	- docker stop $(shell docker ps -qa)
-	- docker rm $(shell docker ps -qa)
-	- docker volume rm $(shell docker volume ls -q)
-	- docker network rm $(shell docker network ls -q)
+	docker system prune -af --volumes
 
 
 re: fclean all
--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,14 +4,10 @@ sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
 sed -i "s/127.0.0.1/0.0.0.0/1" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
-if [ -d /var/lib/mysql/wordpress ]; then
-    echo "wordpress already installed"
-else
     mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
     mariadb -e "CREATE DATABASE \`${MYSQL_DATABASE}\` ;"
     mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
     mariadb -e "GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
     mariadb -e "FLUSH PRIVILEGES;"
-fi
 service mariadb stop
 exec "$@"
