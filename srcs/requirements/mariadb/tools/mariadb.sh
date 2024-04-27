b3209240015491c85936894ec7e83bf829070ae0
luna <77958812+linasbk@users.noreply.github.com>
2024-04-27 17:42:51 +0100

    Update mariadb.sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,6 +3,7 @@
 sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
 sed -i "s/127.0.0.1/0.0.0.0/1" /etc/mysql/mariadb.conf.d/50-server.cnf
 
+
 service mariadb start
     mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
     mariadb -e "CREATE DATABASE \`${MYSQL_DATABASE}\` ;"
